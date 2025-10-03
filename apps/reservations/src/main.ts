51c098f77a30e65b0192790db3b34b1bf6822f09
MD ARIFUL HASAN <mdahtotul@gmail.com>
2025-10-03 17:38:51 +0600

    feat: crud functionality validation and logging for reservations

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { ReservationsModule } from './reservations.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   const port = process.env.port ?? 3000;
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(port);
 
   console.log(`Application is running on: http://localhost:${port}`);
